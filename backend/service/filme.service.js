6206992aec2f79e5aaa95b3dee60d01312ac9339
nielsongomes26 <nielsongomes16@gmail.com>
2025-11-07 17:27:05 -0300

    feat(backend): Estrutura API em 5 camadas (Issue #1)

---
+++ backend/service/filme.service.js
@@ -0,0 +1,15 @@
+const filmes = [
+    {
+        id: 1,
+        titulo: 'Exemplo 1',
+        ator: 'Ryan Reynolds',
+        faixaEtaria: 16,
+        genero: 'Ação/Aventura/Comédia'
+    }
+];
+
+const findAll = () => {
+    return filmes;
+};
+
+export default { findAll }
