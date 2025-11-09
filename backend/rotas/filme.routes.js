1b9178ae5a5c7816a130b07c026246b409662fdb
nielsongomes26 <nielsongomes16@gmail.com>
2025-11-08 22:37:55 -0300

    feat(backend): Implementa Prisma e CRUD de Filmes (Issues #2, #3)

--- backend/rotas/filme.routes.js
+++ backend/rotas/filme.routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import filmeController from '../controller/filme.controller.js';
 
-const router = Router();
+const router = Router(); 
 
 router.get('/filmes', filmeController.findAll);
+router.get('/filmes/:id', filmeController.findById);
+router.post('/filmes', filmeController.create);
+router.put('/filmes/:id', filmeController.update);
+router.delete('/filmes/:id', filmeController.remove);
 
 export default router;
